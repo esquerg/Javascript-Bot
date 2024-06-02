8f9465890fbd4cb09a960e7aeb201100965cf763
Emiliano Esquer <emiliano.esquer.g@gmail.com>
2024-06-02 16:17:25 -0400

    Initial Commit

---
+++ src/bot.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+
+//console.log(process.env.TOKEN)
+
+const MY_TOKEN = process.env.TOKEN;
+
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const PREFIX = '!';
+
+const client = new Client({intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers,
+]});
+
+client.on('ready', () => {
+    console.log(`The ${client.user.username} is now online!`)
+})
+
+client.on('messageCreate', (msg) => {
+    console.log(msg.content)
+
+    if (msg.content === 'Hello'){
+        msg.reply(`Oh I see`)
+        msg.channel.send(`Hello ${msg.author.tag}`)
+    }
+})
+
+client.login(MY_TOKEN)
+
+
+
